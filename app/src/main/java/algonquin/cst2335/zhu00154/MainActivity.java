ac09e0e58ab1b2309542153dce7831c0a48abb9d
chaohao1214 <zch264999454@gmail.com>
2021-05-26 13:16:44 -0400

    Finished Phone Version of Lab 3

--- app/src/main/java/algonquin/cst2335/zhu00154/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/zhu00154/MainActivity.java
@@ -3,6 +3,10 @@ package algonquin.cst2335.zhu00154;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageView;
+import android.widget.Switch;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView imageView = findViewById(R.id.flagview);
+        Switch  sw = findViewById(R.id.spin_switch);
+
+        sw.setOnCheckedChangeListener((btn, isChecked) ->{
+            if (isChecked) {
+                RotateAnimation rotate = new RotateAnimation(0,360, Animation.RELATIVE_TO_SELF,0.5f
+                , Animation.RELATIVE_TO_SELF, 0.5f);
+                rotate.setDuration(5000);
+                rotate.setRepeatCount(Animation.INFINITE);
+
+                imageView.startAnimation(rotate);
+            }
+            else {
+                imageView.clearAnimation();
+            }
+        });
+
     }
 }
